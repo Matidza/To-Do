bd2fcdf735a4f0ec6f664e04bf40322c66448243
Matidza <Matidza46@gmail.com>
2024-08-28 10:51:19 +0200

    Track Modifications

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Todo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
 app.config['SECRET_KEY'] = 'dsjvcnw4%*&TBVhv@$#3'
 
-db = SQLAlchemy(app, track_modifications=False)
+db = SQLAlchemy(app)
 #migrate = Migrate(app, db)
 # Creating ( C ) Models ~ Row of data
 class Mytodo(db.Model):
